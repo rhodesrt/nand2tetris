7751cdfd9cb7ecb56084bf9b16e6c97735213f1d
rhodesrt <rhodestrt@gmail.com>
2025-01-21 18:34:09 -0800

    Fill.asm

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,70 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// Screen is 32x16 (16 bit)
+// KBD is either an input or 0
+
+// LOOP
+// Check if key is pressed
+// If pressed, write -1 to all SCREEN registers
+// Else, write 0 to all SCREEN registers
+
+(LOOP)
+@512
+D=A
+@count
+M=D
+@n
+M=0
+@SCREEN
+D=A
+@current
+M=D
+
+@KBD
+D=M
+@WHITE
+D;JEQ
+@BLACK
+D;JNE
+
+(WHITE)
+@n
+D=M
+@count
+D=D-M
+@LOOP
+D;JEQ
+
+@current
+A=M
+M=0
+@current
+M=M+1
+
+@n
+M=M+1
+@WHITE
+0;JMP
+
+(BLACK)
+@n
+D=M
+@count
+D=D-M
+@LOOP
+D;JEQ
+
+@current
+A=M
+M=-1
+@current
+M=M+1
+
+@n
+M=M+1
+@BLACK
+0;JMP
+
+@LOOP
+0;JMP
