dea02ae91eef696bc31ec3627ac396a95ceaea68
Tristan Rhodes <trhodes@launchdarkly.com>
2025-04-14 12:36:29 -0700

    impl pop temp

--- projects/vm_translator/code_writer.py
+++ projects/vm_translator/code_writer.py
@@ -262,7 +262,21 @@ class ASMCodeWriter:
           "M=D"
         ]
       elif segment == "temp":
-        instructions = []
+        instructions = [
+          f"@{index}",
+          "D=A",
+          "@5",
+          "D=A+D",
+          "@R13",
+          "M=D",
+          "@SP",
+          "M=M-1",
+          "A=M",
+          "D=M",
+          "@R13",
+          "A=M",
+          "M=D"
+        ]
       elif segment == "arg":
         instructions = []
       elif segment == "this":
