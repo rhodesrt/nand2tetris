c7dec0573f85278fd39541981170e8b34e7e7e47
rhodesrt <rhodestrt@gmail.com>
2025-04-25 17:24:51 -0700

    implement return

--- projects/vm_translator/code_writer.py
+++ projects/vm_translator/code_writer.py
@@ -428,6 +428,69 @@ class ASMCodeWriter:
 
   def write_return(self):
     self.write_line("// return")
+    instructions = [
+      # FRAME = LCL
+      "@LCL",
+      "D=M",
+      "@R13",
+      "M=D",
+      # RET = *(FRAME - 5)
+      "@5",
+      "A=D-A",
+      "D=M",
+      "@R14",
+      "M=D",
+      # *ARG = pop()
+      "@SP",
+      "M=M-1",
+      "A=M",
+      "D=M",
+      "@ARG",
+      "A=M",
+      "M=D",
+      # SP = ARG + 1
+      "@ARG",
+      "D=M+1",
+      "@SP",
+      "M=D",
+      # THAT = *(FRAME - 1)
+      "@R13",
+      "A=M-1",
+      "D=M",
+      "@THAT",
+      "M=D",
+      # THIS = *(FRAME - 2)
+      "@R13",
+      "D=M",
+      "@2",
+      "A=D-A",
+      "D=M",
+      "@THIS",
+      "M=D",
+      # ARG = *(FRAME - 3)
+      "@R13",
+      "D=M",
+      "@3",
+      "A=D-A",
+      "D=M",
+      "@ARG",
+      "M=D",
+      # LCL = *(FRAME - 4)
+      "@R13",
+      "D=M",
+      "@4",
+      "A=D-A",
+      "D=M",
+      "@LCL",
+      "M=D",
+      # goto RET
+      "@R14",
+      "A=M",
+      "0;JMP",
+    ]
+
+    for ins in instructions:
+      self.write_line(ins)
 
   def loop(self):
     self.write_line("// Infinite loop")
